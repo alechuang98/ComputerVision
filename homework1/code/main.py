ea462b522596dc1a79d4070102dc8c2bfb5126f6
alechuang98 <alechuang98@gmail.com>
2019-09-13 22:05:49 +0800

    ver 1.0

---
+++ homework1/code/main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("lena.bmp")
+upsideDown = np.zeros(image.shape)
+rightsideLeft = np.zeros(image.shape)
+diagonallyMirrored = np.zeros(image.shape)
+
+lenR = image.shape[0]
+lenC = image.shape[1]
+for r in range(lenR):
+    for c in range(lenC):
+        upsideDown[lenR - r - 1, c, :] = image[r, c, :]
+        rightsideLeft[r, lenC - c - 1, :] = image[r, c, :]
+        diagonallyMirrored[c, r, :] = image[r, c, :]
+
+cv2.imwrite("upside-down lena.bmp", upsideDown)
+cv2.imwrite("right-side-left lena.bmp", rightsideLeft)
+cv2.imwrite("diagonally mirrored lena.bmp", diagonallyMirrored)
