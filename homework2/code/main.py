12f16d7421fe44a935252af5a13da87745c83b17
alechuang98 <alechuang98@gmail.com>
2019-09-20 17:16:49 +0800

    hw2 ver2

--- homework2/code/main.py
+++ homework2/code/main.py
@@ -1,13 +1,35 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+inf = 10000000
+
+l, r, d, u
+def dfs(row, col, vis, img):
+    global l, r, d, u
+    if row < 0 or row > 511 or col < 0 or col > 511 or vis[row, col] == 1 or img[row, col] != 0:
+        return
+    l = min(l, col)
+    r = max(r, col)
+    d = min(d, row)
+    u = max(u, row)
+    dfs(row + 1, col, vis, img)
+    dfs(row - 1, col, vis, img)
+    dfs(row, col + 1, vis, img)
+    dfs(row, col - 1, vis, img)
 
 image = cv2.imread("lena.bmp")
 binary = (image >= 128) * np.full(image.shape, 255)
-histo = image[:, :, 0].copy()
-plt.hist(histo.reshape(histo.size), bins=range(256))
+img2D = image[:, :, 0].copy()
+plt.hist(img2D.reshape(img2D.size), bins=range(256))
 
-lenR = image.shape[0]
-lenC = image.shape[1]
+vis = np.zeros(img2D.shape)
+
+for r in range(vis.shape[0]):
+    for c in range(vis.shape[1]):
+        if vis[r, c] == 0:
+            l, r, d, u = inf, -inf, inf, -inf
+            dfs(r, c, vis, binary[:, :, 0])
+            
 
 cv2.imwrite("1_binary lena.bmp", binary)
+plt.savefig("2_histogram.png")
